dad069f40d54dbbd94eaaf848580763bec243190
Eugene Trounev <eugene.trounev@gmail.com>
2015-05-21 02:26:25 -0400

    Ne smaller margin size

--- Common/sizes.js
+++ Common/sizes.js
@@ -17,6 +17,7 @@ var FONT_MARGIN_HEADLINE = FONT_MARGIN * 1.25;
 var FONT_MARGIN_DISPLAY = FONT_MARGIN_HEADLINE;
 //Spacing
 var MARGIN = U.px(16);
+var MARGIN_HALF = MARGIN / 2;
 var MARGIN_DOUBLE = MARGIN * 2;
 var BORDER = U.px(1);
 var BORDER_DOUBLE = BORDER * 2;
@@ -25,7 +26,7 @@ var ICON = U.px(24);
 var ICON_32 = U.px(32);
 var ICON_64 = ICON_32 * 2;
 //Toolbar
-var TOOLBAR = U.px(64);
+var TOOLBAR = U.px(60);
 var TOOLBAR_MIN = TOOLBAR;
 var TOOLBAR_MAX = TOOLBAR * 2;
 var SUBTOOLBAR = U.px(48);
